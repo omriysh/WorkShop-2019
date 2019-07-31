336d9166df71a3fc02a9a46b644f9434a0f7c35f
Omri <omri.ysh@gmail.com>
2019-07-31 19:09:52 +0300

    actually added main2 in the cmakelist file :) also made main2 be main1 for now

--- AttackerTest2/main2.cpp
+++ AttackerTest2/main2.cpp
@@ -2,3 +2,42 @@
 // Created by User on 31/7/2019.
 //
 
+#include <string.h>
+#include <iostream>
+#include "../Library/Measurements.h"
+#include "../Library/FRAttacker.h"
+#include "../Library/MemoryAccess.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    // declaring variables
+    string path = "/mnt/c/Users/User/Documents/Arazim/Workshop/WorkShop-2019/testing/test2victim";
+    char target[] = {'\xc7', '\x45', '\xfc', '\x06', '\x00'};
+    Measurements measurements;
+
+    //logic
+    cout << "We are up and running! :)" << endl;
+
+    FRAttacker attacker = FRAttacker(path, target, 5, 1000, 1);
+    attacker.Configure();
+
+    cout << "Attacker configured" << endl;
+
+    while (true)
+    {
+        attacker.Attack();
+        measurements = attacker.GetMeasurements();
+        for (auto& access : measurements.GetAccessVector())
+        {
+            if (access.speculatedCache == Cache::L3)
+            {
+                cout << "Recorded access at time " << access.timeOfMeasure << endl;
+            }
+        }
+    }
+
+    //wrap up and return
+    return 0;
+}
