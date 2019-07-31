2293be9b562ae77d5ff97bdb7e3a93fc74efe720
Omri <omri.ysh@gmail.com>
2019-07-31 17:47:37 +0300

    added helper function "exec", and also added some std::move stuff that clang wanted for some reason ¯\_(ツ)_/¯
    (no idea if this actually works yet, but it should at the very least compile)

--- Library/PPAttacker.cpp
+++ Library/PPAttacker.cpp
@@ -1,20 +1,52 @@
+#include <utility>
+
+#include <utility>
+
+#include <utility>
+
 //
 // Created by User on 18/5/2019.
 //
 
 #include "PPAttacker.h"
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+
+// ~~~~~~~ helper functions ~~~~~~~
+
+// this function executes a console command and returns the output (stdout only)
+string exec(const char* cmd) {
+    array<char, 128> buffer{};
+    string result;
+    unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
+
+// ~~~~~~~ PPAttacker functions ~~~~~~~
 
 PPAttacker::PPAttacker(string path) :
-        Attacker(path) {}
+        Attacker(move(path)) {}
 
 PPAttacker::PPAttacker(string path, char* target, int len) :
-        Attacker(path, target, len) {}
+        Attacker(move(path), target, len) {}
 
 PPAttacker::PPAttacker(string path, char* target, int len, int intervalTime) :
-        Attacker(path, target, len, intervalTime) {}
+        Attacker(move(path), target, len, intervalTime) {}
 
 PPAttacker::PPAttacker(string path, char* target, int len, int intervalTime, int iteration) :
-        Attacker(path, target, len, intervalTime, iteration) {}
+        Attacker(move(path), target, len, intervalTime, iteration) {}
 
 
 void PPAttacker::Attack() {
